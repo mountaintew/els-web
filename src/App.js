519915ddbf60cc663c93bab6e878cc78c73d92e4
Les <lesteraranaaice@gmail.com>
2021-05-19 05:47:23 +0800

    added theme & font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import React from 'react';
 import './App.css';
+import { createMuiTheme, ThemeProvider, Typography }  from '@material-ui/core'  
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { amber } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: amber,
+  },
+  typography: {
+    fontFamily: 'Montserrat',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700
+  }  
+})
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
-    
+      <Typography variant="h4">Hello World</Typography>
     </div>
+    </ThemeProvider>
   );
 }
 
